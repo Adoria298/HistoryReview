0794d05f9b86cb53b49ff89e5e7822c69ebe17a3
Adoria298 <bananaorange29@outlook.com>
2020-08-12 18:08:32 +0100

    More analysis of the SQL.

--- get_history.py
+++ get_history.py
@@ -12,7 +12,15 @@ db_file = copyfile(user_data_dir("Chrome", "Google") + \
 conn = sqlite3.connect(db_file)
 cursor = conn.cursor()
 
-cursor.execute("SELECT * FROM urls LIMIT 10")
+#cursor.execute("SELECT name from sqlite_master WHERE type='table'")
+#pprint(cursor.fetchall())
+
+cursor.execute("SELECT * FROM urls LIMIT 20")
+print([description[0] for description in cursor.description])
+pprint(cursor.fetchall())
+
+cursor.execute("SELECT id, url, visit_count FROM urls LIMIT 20")
+print([description[0] for description in cursor.description])
 pprint(cursor.fetchall())
 
 # final actions
