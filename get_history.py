8ddc7ec2c4736a962c579b34a16fcabfd08bb62d
Adoria298 <bananaorange29@outlook.com>
2020-08-13 17:30:18 +0100

    Added comments.

--- get_history.py
+++ get_history.py
@@ -1,25 +1,36 @@
 import sqlite3
 from shutil import copyfile
 from pprint import pprint
-from appdirs import user_cache_dir, user_data_dir
 from datetime import datetime
 
+from appdirs import user_cache_dir, user_data_dir
+
+# copy file because Chrome locks the database when open
+# currently the user's responsibility to delete these files
+# may not work on other platforms - never been tested
 db_file = copyfile(user_data_dir("Chrome", "Google") + \
         "\\User Data\\Default\\History", 
     user_cache_dir("HistoryReview", "Adoria298") + \
         "\Chrome_History_Copy_" + \
         str(datetime(2020, 8, 12).now().toordinal()))
+
+# set sqlite by connecting to the copied file
 conn = sqlite3.connect(db_file)
 cursor = conn.cursor()
 
+# find the names of the tables
 #cursor.execute("SELECT name from sqlite_master WHERE type='table'")
 #pprint(cursor.fetchall())
 
-cursor.execute("SELECT * FROM urls LIMIT 20")
-print([description[0] for description in cursor.description])
-pprint(cursor.fetchall())
+#cursor.execute("SELECT * FROM urls LIMIT 20")
+# prints the coloumn header
+#TODO: make this output prettier
+#print([description[0] for description in cursor.description]) 
+#pprint(cursor.fetchall())
+
 
 cursor.execute("SELECT id, url, visit_count FROM urls LIMIT 20")
+# note how there are less headers
 print([description[0] for description in cursor.description])
 pprint(cursor.fetchall())
 
